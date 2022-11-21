8f57ae1caf9b8a81b81db7816e0823ffa3a8cb52
gasser <gasserkhaled23@gmail.com>
2022-11-21 16:21:15 +0200

    update tests of Image class

--- src/tests/ImageSpec.ts
+++ src/tests/ImageSpec.ts
@@ -1,17 +1,19 @@
 import Image from '../utilities/Image';
 
 describe('Testing image processing', () => {
-    let imageObj: Image;
-    beforeEach(() => {
-        imageObj = new Image(200,200);
-    })
-    it('Throws a missing input error if the wrong filename is provided',  async () => {
-        const result = imageObj.resizeImage('alaska');
+    let imageObj1: Image;
+    let imageObj2: Image;
+    beforeAll(() => {
+        imageObj1 = new Image(200, 200, 'alaska');
+        imageObj2 = new Image(300, 300, 'santamonica');
+    });
+    it('Throws a missing input error if the wrong filename is provided',  async (): Promise<void> => {
+        const result = imageObj1.resizeImage();
         await expectAsync(result).toBeRejected();
     });
 
-    it('Expect Transform to not throw error or error: Input File is missing', async () => {
-        const result = imageObj.resizeImage('santamonica');
+    it('Expect Transform to not throw error or error: Input File is missing', async (): Promise<void> => {
+        const result = imageObj2.resizeImage();
         await expectAsync(result).toBeResolved();
     });
 });
