37bda4416a4a765c04ea9648375ab5a4387c0ece
gasser <gasserkhaled23@gmail.com>
2022-11-21 16:21:38 +0200

    make refactor to Image class

--- src/utilities/Image.ts
+++ src/utilities/Image.ts
@@ -6,36 +6,24 @@ import path from 'path';
 class Image {
     private readonly _width: number;
     private readonly _height: number;
+    private readonly _imageName: string;
 
-    constructor(width: number, height: number) {
+    constructor(width: number, height: number, _imageName: string) {
         this._width = width;
         this._height = height;
+        this._imageName = _imageName;
     }
 
-    get width(): number {
-        return this._width;
-    }
-
-    get height(): number {
-        return this._height;
-    }
-
-// @ts-ignore
-    resizeImage(fileName: string): Promise<sharp.OutputInfo> {
-
-        const inputPath: string = path.resolve(`public/images/${fileName}${'.jpg' || '.png'}`);
-        try {
-            return sharp(inputPath)
-                .resize({
-                    width: this._width,
-                    height: this._height
-                })
-                .toFormat("jpeg", {mozjpeg: true})
-                .toFile(`public/processed-images/${fileName}${this._width}x${this._height}.jpeg`);
-        } catch (error) {
-            // @ts-ignore
-            console.log(error.message);
-        }
+    resizeImage(): Promise<sharp.OutputInfo>  {
+        const inputPath: string = path.resolve(`public/images/${this._imageName}${'.jpg' || '.png'}`);
+        
+        return sharp(inputPath)
+            .resize({
+                width: this._width,
+                height: this._height
+            })
+            .toFormat('jpeg', {mozjpeg: true})
+            .toFile(`public/processed-images/${this._imageName}${this._width}x${this._height}.jpeg`);
     }
 }
 
