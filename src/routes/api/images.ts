4226b0e23d744d82850b6dcc51fd70c8529c170a
gasser <gasserkhaled23@gmail.com>
2022-11-22 08:32:26 +0200

    fix handling Errors of invalid height and width

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,4 +1,4 @@
-import express , {Request, Response} from 'express';
+import express, {Request, Response} from 'express';
 import path from 'path';
 import Image from '../../utilities/Image';
 import fs from 'fs';
@@ -6,23 +6,26 @@ import fs from 'fs';
 const images = express.Router();
 
 images.get('/', async (req: Request, res: Response): Promise<void> => {
-  try {
-    const filename = req.query.filename as string;
-    const imageHeight = parseInt(req.query.height as string);
-    const imageWidth = parseInt(req.query.width as string);
-    const ImgPath = path.resolve(
-      `./public/processed-images/${filename}${imageWidth}x${imageHeight}.jpeg`);
-    
-    if (!fs.existsSync(ImgPath)) {
-      const ImageObj = new Image(imageWidth, imageHeight, filename);
-      await ImageObj.resizeImage();
-      res.sendFile(ImgPath);
-    } 
-    res.sendFile(ImgPath);
-  } catch (e) {
-    res.render('error', { message: 'Input File is Missing!' });
-    throw new Error('Input File is Missing!');
-  }
+    try {
+        const filename = req.query.filename as string;
+        const imageHeight = parseInt(req.query.height as string);
+        const imageWidth = parseInt(req.query.width as string);
+        if (Number(req.query.height) === imageHeight && Number(req.query.width) === imageWidth) {
+            const ImgPath = path.resolve(`./public/processed-images/${filename}${imageWidth}x${imageHeight}.jpeg`);
+
+            if (!fs.existsSync(ImgPath)) {
+                const ImageObj = new Image(imageWidth, imageHeight, filename);
+                await ImageObj.resizeImage();
+                res.sendFile(ImgPath);
+            }
+            res.sendFile(ImgPath);
+        } else {
+            res.render('error', {message: 'Error: please Enter a valid number for width and height'});
+        }
+    } catch (e) {
+        res.render('error', {message: 'Input File is Missing!'});
+        throw new Error('Input File is Missing!');
+    }
 });
 
 export default images;
--- src/utilities/Image.ts
+++ src/utilities/Image.ts
@@ -1,8 +1,6 @@
 import sharp from 'sharp';
 import path from 'path';
 
-//import { promises as fsPromises } from 'fs';
-
 class Image {
     private readonly _width: number;
     private readonly _height: number;
@@ -14,16 +12,18 @@ class Image {
         this._imageName = _imageName;
     }
 
-    resizeImage(): Promise<sharp.OutputInfo>  {
+    resizeImage(): Promise<sharp.OutputInfo> {
         const inputPath: string = path.resolve(`public/images/${this._imageName}${'.jpg' || '.png'}`);
-        
-        return sharp(inputPath)
-            .resize({
-                width: this._width,
-                height: this._height
-            })
-            .toFormat('jpeg', {mozjpeg: true})
-            .toFile(`public/processed-images/${this._imageName}${this._width}x${this._height}.jpeg`);
+        try {
+            return sharp(inputPath)
+                .resize({
+                    width: this._width, height: this._height
+                })
+                .toFormat('jpeg', {mozjpeg: true})
+                .toFile(`public/processed-images/${this._imageName}${this._width}x${this._height}.jpeg`);
+        } catch (e) {
+            throw new Error('Failed in resizing Image');
+        }
     }
 }
 
